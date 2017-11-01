d3a8f56e6b3eec43cd0f08a64d9de72160f6dfe2
Chris Gibb <chris.gibb@outlook.com>
2017-11-01 17:55:24 -0400

    parse proc file

--- Q1/roundrobin.hpp
+++ Q1/roundrobin.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <list>
+#include <string>
+#include <cstring>
+#include <iostream>
 #include "process.hpp"
 class RoundRobin
 {
@@ -10,6 +13,36 @@ class RoundRobin
         template<class T>
         bool loadFromStream(T&stream)
         {
+            int processes = 0;
+            std::string lineStr;
+            std::getline(stream,lineStr);
+            char*line = (char*)lineStr.c_str();
+            {
+                char*split;
+                split = std::strtok(line," ");
+                processes = std::atoi(split);
+
+                split = std::strtok(NULL," ");
+                this->timeQuantum = std::atoi(split);
+            }
+            for(int i = 0; i != processes; ++i)
+            {
+                std::getline(stream,lineStr);
+                line = (char*)lineStr.c_str();
+                {
+                    char*split;
+                    ::Process proc;
+                    split = std::strtok(line," ");
+                    proc.id = std::atoi(split);
+                    split = std::strtok(NULL," ");
+                    proc.priority = std::atoi(split);
+                    split = std::strtok(NULL," ");
+                    proc.burstTime = std::atoi(split);
+                    split = std::strtok(NULL," ");
+                    proc.arrivalTime = std::atoi(split);
+                    this->processes.push_back(proc);
+                }
+            }
             return true;
         }
         
