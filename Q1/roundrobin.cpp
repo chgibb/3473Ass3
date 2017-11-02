0244d2cddf8d999caec2ee7348e42348c1974331
Chris Gibb <chris.gibb@outlook.com>
2017-11-02 10:31:48 -0400

    sort not arrived procs

--- Q1/roundrobin.cpp
+++ Q1/roundrobin.cpp
@@ -1,9 +1,11 @@
 #include <stdexcept>
+#include <list>
 #include "roundrobin.hpp"
 void ::RoundRobin::runQueueWithProcesses()
 {
     int ticks = 0;
     this->procQueue.clear();
+    std::list<::Process*> notArrived;
     {
         auto procEnd = this->processes.end();
         for(auto it = this->processes.begin(); it != procEnd; ++it)
@@ -12,7 +14,14 @@ void ::RoundRobin::runQueueWithProcesses()
             {
                 this->procQueue.push_back(&(*it));
             }
+            else
+            {
+                notArrived.push_back(&(*it));
+            }
         }
+        notArrived.sort([this](::Process*&a,::Process*&b) -> bool{
+            return a->arrivalTime < b->arrivalTime;
+        });
     }
     if(this->procQueue.size() == 0)
         throw std::runtime_error("At least one process must have an arrival time of 0");
