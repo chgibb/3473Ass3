195c41cf3e6e5fcf27b92e58179920bc3b0a70b9
Chris Gibb <chris.gibb@outlook.com>
2017-11-02 10:22:35 -0400

    throw on no procs with arrival time of 0

--- Q1/roundrobin.cpp
+++ Q1/roundrobin.cpp
@@ -1,7 +1,21 @@
+#include <stdexcept>
 #include "roundrobin.hpp"
 void ::RoundRobin::runQueueWithProcesses()
 {
-
+    int ticks = 0;
+    this->procQueue.clear();
+    {
+        auto procEnd = this->processes.end();
+        for(auto it = this->processes.begin(); it != procEnd; ++it)
+        {
+            if(it->arrivalTime == 0)
+            {
+                this->procQueue.push_back(&(*it));
+            }
+        }
+    }
+    if(this->procQueue.size() == 0)
+        throw std::runtime_error("At least one process must have an arrival time of 0");
 }
 
 ::RoundRobin::RoundRobin() = default;
