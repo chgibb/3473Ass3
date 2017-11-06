ef0786fa1df8e678635c5a83e86e8c81122dad0a
Chris Gibb <chris.gibb@outlook.com>
2017-11-06 17:55:19 -0500

    take in and out files from the command line

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include "roundrobin.hpp"
 int main(int argc,char*argv[])
 {
-    std::ifstream file("sample.txt",std::ios::in);
+    if(argc < 3)
+    {
+        std::cerr<<"Insufficient arguments"<<std::endl;
+        return 1;
+    }
+    if(argc > 3)
+    {
+        std::cerr<<"Too many arguments"<<std::endl;
+        return 1;
+    }
+    if(std::strcmp(argv[1],argv[2]) == 0)
+    {
+        std::cerr<<"Input and output cannot be the same file"<<std::endl;
+        return 1;
+    }
+    std::ifstream inFile(argv[1],std::ios::in);
 
     ::RoundRobin rr;
 
-    rr.loadFromStream<std::ifstream>(file);
-    file.close();
+    rr.loadFromStream<std::ifstream>(inFile);
+    inFile.close();
 
     rr.runQueueWithProcesses();
 
-    rr.printProcessStats<decltype(std::cout)>(std::cout);
+    std::ofstream outFile(argv[2],std::ios::out);
+
+    rr.printProcessStats<std::ofstream>(outFile);
     return 0;
 }
