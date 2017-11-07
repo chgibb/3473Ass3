4d38ae4bcab1c03c4442d249b3ae02978fb327a2
Chris Gibb <chris.gibb@outlook.com>
2017-11-07 10:43:22 -0500

    commented

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+/*
+    The program will take in the path to a file with the processes to run and the path to a file
+    to output results to.
+*/
 #include <fstream>
 #include <cstring>
 #include "roundrobin.hpp"
+
 /**
  * @brief 
  * 
@@ -13,6 +17,7 @@
  */
 int main(int argc,char*argv[])
 {
+    //take exactly 2 arguments
     if(argc < 3)
     {
         std::cerr<<"Insufficient arguments"<<std::endl;
@@ -23,22 +28,28 @@ int main(int argc,char*argv[])
         std::cerr<<"Too many arguments"<<std::endl;
         return 1;
     }
+
+    //disallow taking input from and outputting to the same file
     if(std::strcmp(argv[1],argv[2]) == 0)
     {
         std::cerr<<"Input and output cannot be the same file"<<std::endl;
         return 1;
     }
+
     std::ifstream inFile(argv[1],std::ios::in);
 
     ::RoundRobin rr;
 
+    //populate processes for round robin from input file
     rr.loadFromStream<std::ifstream>(inFile);
     inFile.close();
 
+    //run round robin
     rr.runQueueWithProcesses();
 
     std::ofstream outFile(argv[2],std::ios::out);
 
+    //output results
     rr.printProcessStats<std::ofstream>(outFile);
     return 0;
 }
