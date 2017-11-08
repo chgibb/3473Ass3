498216744b88199d6231485605eff803c2cfb7af
Chris Gibb <chris.gibb@outlook.com>
2017-11-08 12:48:06 -0500

    removed hardcoded matrixes

--- Q2/main.cpp
+++ Q2/main.cpp
@@ -5,37 +5,6 @@
 
 int main(int argc,char*argv[])
 {
-  /*  Comp3473Ass3::Bankers bankers(5,4);
-    std::vector<int> allocMatrix{
-        2,0,0,1,
-        3,1,2,1,
-        2,1,0,3,
-        1,3,1,2,
-        1,4,3,2
-    };
-
-    std::vector<int> maxMatrix{
-        4,2,1,2,
-        5,2,5,2,
-        2,3,1,6,
-        1,4,2,4,
-        3,6,6,5
-    };
-
-    std::vector<int> availMatrix{
-        3,3,2,1
-    };
-
-    bankers.setMatrix(bankers.allocatedResources,allocMatrix);
-    bankers.setMatrix(bankers.maxResources,maxMatrix);
-    bankers.setAvailableMatrix(availMatrix);
-
-    bool isSafe = bankers.isSafe();
-    if(isSafe)
-        std::cout<<"Safe state"<<std::endl;
-    else
-        std::cout<<"Unsafe state"<<std::endl;*/
-
     int processes = 0;
     int resources = 0;
     std::ifstream inFile(argv[1],std::ios::in);
@@ -48,6 +17,7 @@ int main(int argc,char*argv[])
         split = std::strtok(NULL," ");
         resources = std::atoi(split);
     }
+
     Comp3473Ass3::Bankers bankers(processes,resources);
     bankers.loadFromStream<std::ifstream>(inFile);
 
