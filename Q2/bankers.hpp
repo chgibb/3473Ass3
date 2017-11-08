1a30f3dc251dfd5dcd77f4f211568c12ee326f16
Chris Gibb <chris.gibb@outlook.com>
2017-11-08 12:45:22 -0500

    load matrixes from file

--- Q2/bankers.hpp
+++ Q2/bankers.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <string>
+#include <cstring>
 #include <vector>
-
+#include <iostream>
 namespace Comp3473Ass3
 {
     /**
@@ -20,6 +22,18 @@ namespace Comp3473Ass3
             void setMatrix(std::vector<std::vector<int>>&,std::vector<int>&);
             void setAvailableMatrix(std::vector<int>&);
             bool isSafe();
+            template <class T>
+            bool loadFromStream(T&stream)
+            {
+                std::vector<int> streamMatrix = this->getMatrixFromStream<decltype(stream)>(stream);
+                this->setMatrix(this->allocatedResources,streamMatrix);
+
+                streamMatrix = this->getMatrixFromStream<decltype(stream)>(stream);
+                this->setMatrix(this->maxResources,streamMatrix);
+
+                streamMatrix = this->getAvailableMatrixFromStream<decltype(stream)>(stream);
+                this->setAvailableMatrix(streamMatrix);
+            }
             Bankers(int,int);
             ~Bankers();
 
@@ -29,5 +43,59 @@ namespace Comp3473Ass3
 
             std::vector<std::vector<int>>&calculateRequired();
             bool check(int);
+            template<class T>
+            std::vector<int> getMatrixFromStream(T&stream)
+            {
+                std::vector<int> res;
+
+                std::string lineStr;
+                for(int i = 0; i != this->processes+1; ++i)
+                {
+                    char*split = nullptr;
+                    char*line = nullptr;
+
+                    std::getline(stream,lineStr);
+                    line = (char*)lineStr.c_str();
+                    split = std::strtok(line," ");
+
+                    if(split)
+                        res.push_back(std::atoi(split));
+                        
+                    for(int j = 0; j != this->resources; ++j)
+                    {
+                        split = std::strtok(NULL," ");
+                        if(split)
+                            res.push_back(std::atoi(split));
+                    }
+                }
+                return res;
+            }
+
+            template<class T>
+            std::vector<int> getAvailableMatrixFromStream(T&stream)
+            {
+                std::vector<int> res;
+
+                std::string lineStr;
+                char*split = nullptr;
+                char*line = nullptr;
+
+                std::getline(stream,lineStr);
+
+                line = (char*)lineStr.c_str();
+                split = std::strtok(line," ");
+
+                if(split)
+                    res.push_back(std::atoi(split));
+                else
+                    return this->getAvailableMatrixFromStream<decltype(stream)>(stream);
+                for(int i = 0; i != this->resources; ++i)
+                {
+                    split = std::strtok(NULL," ");
+                    if(split)
+                        res.push_back(std::atoi(split));
+                }
+                return res;
+            }
     };
 }
--- Q2/main.cpp
+++ Q2/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <fstream>
 
 #include "bankers.hpp"
 
 int main(int argc,char*argv[])
 {
-    Comp3473Ass3::Bankers bankers(5,4);
+  /*  Comp3473Ass3::Bankers bankers(5,4);
     std::vector<int> allocMatrix{
         2,0,0,1,
         3,1,2,1,
@@ -33,6 +34,28 @@ int main(int argc,char*argv[])
     if(isSafe)
         std::cout<<"Safe state"<<std::endl;
     else
+        std::cout<<"Unsafe state"<<std::endl;*/
+
+    int processes = 0;
+    int resources = 0;
+    std::ifstream inFile(argv[1],std::ios::in);
+    {
+        std::string lineStr;
+        std::getline(inFile,lineStr);
+        char*line = (char*)lineStr.c_str();
+        char*split = std::strtok(line," ");
+        processes = std::atoi(split);
+        split = std::strtok(NULL," ");
+        resources = std::atoi(split);
+    }
+    Comp3473Ass3::Bankers bankers(processes,resources);
+    bankers.loadFromStream<std::ifstream>(inFile);
+
+    bool isSafe = bankers.isSafe();
+    if(isSafe)
+        std::cout<<"Safe state"<<std::endl;
+    else
         std::cout<<"Unsafe state"<<std::endl;
+    
     return 0;
 }
