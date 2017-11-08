5f1f7172f36f0c590c3c731e2e1e95379632f977
Chris Gibb <chris.gibb@outlook.com>
2017-11-08 08:53:35 -0500

    moved methods

--- Q2/bankers.cpp
+++ Q2/bankers.cpp
@@ -30,4 +30,27 @@
     
 }
 
+void ::Bankers::setMatrix(std::vector<std::vector<int>>&matrix,std::vector<int>&con)
+{
+    int k = 0;
+    for(int i = 0; i < this->processes; ++i)
+    {
+        for(int j = 0; j < this->resources; ++j)
+        {
+            matrix[i][j] = con[k];
+            ++k;
+        }
+    }
+}
+
+void ::Bankers::setAvailableMatrix(std::vector<int>&con)
+{
+    int k = 0;
+    for(int i = 0; i < this->resources; ++i)
+    {
+        this->availResources[0][i] = con[k];
+        ++k;
+    }
+}
+
 ::Bankers::~Bankers() = default;
--- Q2/bankers.hpp
+++ Q2/bankers.hpp
@@ -3,38 +3,17 @@
 
 class Bankers
 {
-
     public:
         std::vector<std::vector<int>> maxResources;
         std::vector<std::vector<int>> allocatedResources;
         std::vector<std::vector<int>> availResources;
         std::vector<std::vector<int>> requiredResources;
 
-        void setMatrix(std::vector<std::vector<int>>&matrix,std::vector<int>&con)
-        {
-            int k = 0;
-            for(int i = 0; i < this->processes; ++i)
-            {
-                for(int j = 0; j < this->resources; ++j)
-                {
-                    matrix[i][j] = con[k];
-                    ++k;
-                }
-            }
-        }
-
-        void setAvailableMatrix(std::vector<int>&con)
-        {
-            int k = 0;
-            for(int i = 0; i < this->resources; ++i)
-            {
-                this->availResources[0][i] = con[k];
-                ++k;
-            }
-        }
-
+        void setMatrix(std::vector<std::vector<int>>&,std::vector<int>&);
+        void setAvailableMatrix(std::vector<int>&);
         Bankers(int,int);
         ~Bankers();
+        
     private:
         int processes = 0;
         int resources = 0;
