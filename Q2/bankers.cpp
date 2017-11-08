d9331dc396dc39ec6355af10a2e17394701dd935
Chris Gibb <chris.gibb@outlook.com>
2017-11-08 08:54:19 -0500

    fixed method order

--- Q2/bankers.cpp
+++ Q2/bankers.cpp
@@ -1,5 +1,28 @@
 #include "bankers.hpp"
 
+void ::Bankers::setMatrix(std::vector<std::vector<int>>&matrix,std::vector<int>&con)
+{
+    int k = 0;
+    for(int i = 0; i < this->processes; ++i)
+    {
+        for(int j = 0; j < this->resources; ++j)
+        {
+            matrix[i][j] = con[k];
+            ++k;
+        }
+    }
+}
+
+void ::Bankers::setAvailableMatrix(std::vector<int>&con)
+{
+    int k = 0;
+    for(int i = 0; i < this->resources; ++i)
+    {
+        this->availResources[0][i] = con[k];
+        ++k;
+    }
+}
+
 ::Bankers::Bankers(int processes,int resources) : processes(processes),resources(resources)
 {
     this->maxResources = std::vector<std::vector<int>>(
@@ -28,29 +51,6 @@
     );
 
     
-}
-
-void ::Bankers::setMatrix(std::vector<std::vector<int>>&matrix,std::vector<int>&con)
-{
-    int k = 0;
-    for(int i = 0; i < this->processes; ++i)
-    {
-        for(int j = 0; j < this->resources; ++j)
-        {
-            matrix[i][j] = con[k];
-            ++k;
-        }
-    }
-}
-
-void ::Bankers::setAvailableMatrix(std::vector<int>&con)
-{
-    int k = 0;
-    for(int i = 0; i < this->resources; ++i)
-    {
-        this->availResources[0][i] = con[k];
-        ++k;
-    }
 }
 
 ::Bankers::~Bankers() = default;
